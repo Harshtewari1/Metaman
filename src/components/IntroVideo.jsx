98e23bf87bc5e7899f32eaa9808b515f0bf916e4
Harsh <harshtewari2001@gmail.com>
2025-07-25 11:30:51 +0530

    updad65

--- src/components/IntroVideo.jsx
+++ src/components/IntroVideo.jsx
@@ -1,7 +1,7 @@
 
 import { useEffect, useRef } from "react";
 // import introV from "../assets/introV.mp4";
-import introV from "../assets/introV.webm"; 
+import introV from "../assets/IntroV.webm"; 
 
 const IntroVideo = ({ onFinish }) => {
     const wrapperRef = useRef(null);
